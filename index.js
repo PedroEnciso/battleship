079efb0d4f0a38abdd96903f26e661e7184a8829
Pedro Enciso <penciso@ucsd.edu>
2021-07-22 21:19:14 -0700

    create and test Ship object

---
+++ index.js
@@ -0,0 +1,32 @@
+const Ship = (shipLength, shipHits, hasSunk) => {
+  const length = shipLength;
+  const hitIndex = shipHits;
+  const sunk = hasSunk;
+  const hit = (index) => {
+    for (let i = 0; i < length; i++) {
+      if (i === index) {
+        hitIndex[i] = 1;
+      }
+    }
+  };
+  const isSunk = () => {
+    let totalHits = 0;
+    for (let i = 0; i < length; i++) {
+      totalHits += hitIndex[i];
+    }
+    if (totalHits === length) return true;
+    return false;
+  };
+  return {
+    length,
+    hitIndex,
+    sunk,
+    hit,
+    isSunk,
+  };
+};
+
+// functions to export
+module.exports = {
+  Ship,
+};
---
+++ index.test.js
@@ -0,0 +1,20 @@
+const battleship = require("./index");
+
+const someShip = battleship.Ship(4, [0, 0, 1, 0], false);
+
+describe("testing Ship object", () => {
+  test("test Ship.hit()", () => {
+    someShip.hit(1);
+    expect(someShip.hitIndex).toEqual([0, 1, 1, 0]);
+  });
+
+  test("test Ship.isSunk()", () => {
+    expect(someShip.isSunk()).toBe(false);
+  });
+
+  test("test Ship.isSunk() after sinking", () => {
+    someShip.hit(0);
+    someShip.hit(3);
+    expect(someShip.isSunk()).toBe(true);
+  });
+});
