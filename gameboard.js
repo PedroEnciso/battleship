770b8a61c7c2a27e85f58fa9aa204f84f4b3cf18
Pedro Enciso <penciso@ucsd.edu>
2021-07-22 21:58:09 -0700

    create gameboard, gameboard.test and npm script

---
+++ gameboard.js
@@ -0,0 +1,12 @@
+const GameBoard = () => {};
+
+module.exports = GameBoard;
+
+// place ships at specific coordinates
+
+// recieveAttack that takes coordinates and determines whether or not the attack hit a ship. calls hit function
+// record the coordinates of missed shots
+
+// keeps track of missed shots
+
+// report if all ships have been sunk
---
+++ gameboard.test.js
@@ -0,0 +1,10 @@
+const gameboard = require("./gameboard");
+
+// place ships at specific coordinates
+
+// recieveAttack that takes coordinates and determines whether or not the attack hit a ship. calls hit function
+// record the coordinates of missed shots
+
+// keeps track of missed shots
+
+// report if all ships have been sunk
